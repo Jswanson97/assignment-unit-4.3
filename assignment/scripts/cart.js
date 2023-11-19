7f78f8c7ddb77bc6a79719fd7797db7573b43a33
Jesse Swanson <jswanson97@gmail.com>
2023-11-19 14:59:42 -0600

    finished assignment with the help of some friends

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,14 +1,29 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
+var basket = [];
 
+console.log(`Basket is ${basket}`);
 
+function addItem(item){
+    console.log('in addItem');
+    basket.push(item);
+    return true;
+}
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log('Adding bananas (expect true)', addItem('bananas'));
 
+function listItems(){
+    basket.forEach((element) => console.log(element));
+}
+console.log(`Basket is now ${basket}`);
 
-
-
-
+function empty(){
+    console.log(`Basket before empty ${basket}`);
+    basket.length = 0;
+    console.log(`Basket after empty ${basket}`);
+}
+empty();
 
 
 
